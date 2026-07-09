0a5108be269bbf88c9b55a8429a0fdfef2772294
dubeyprakhar601-crypto <dubeyprakhar601@gmail.com>
2026-07-09 16:45:28 +0530

    Time: 47 ms (65.30%) | Memory: 29 MB (92.71%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            int st=i+1;
+            int end=n-1;
+            int tar=-1*nums[i];
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            while(st<end){
+
+                if(nums[st]+nums[end]==tar){
+
+                    ans.push_back({nums[i],nums[st],nums[end]});
+                    st++;
+                    end--;
+                    while(st<end && nums[st-1]==nums[st]){
+                        st++;
+                    }
+                    while(st<end && nums[end]==nums[end+1]){
+                        end--;
+                    }
+                }
+                else if(tar<nums[st]+nums[end]){
+                    end--;
+                }
+                else{
+                    st++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
