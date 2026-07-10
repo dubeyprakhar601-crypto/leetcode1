19296a903dd4b9942c3901c816462842c4d14e2f
dubeyprakhar601-crypto <dubeyprakhar601@gmail.com>
2026-07-10 12:18:24 +0530

    Time: 39 ms (72.49%) | Memory: 45.5 MB (56.39%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,25 +3,27 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         int count=0;
-        for(int i=0;i<n;i++){
-            int j=i;
-            int sum=0;
-            while(j<n){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                    j++;
-                }
-                else{
-                    j++;
-                }
+        int i=1;
+        
+        while(i<n){
+            nums[i]=nums[i]+nums[i-1];
+            i++;
+        }
+        unordered_map<int,int>m;
+        
+        for(int j=0;j<n;j++){
+
+            if(nums[j]==k) count++;
+
+            int val=nums[j]-k;
+
+            if(m.find(val)!=m.end()){
+                count+=m[val];
+            }else{
+                m[nums[j]]==0;
             }
-
-
+            m[nums[j]]++;
         }
         return count;
-
-
-        
     }
 };
