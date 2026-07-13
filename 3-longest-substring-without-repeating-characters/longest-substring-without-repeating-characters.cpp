c5ae769e83af1c720036c5974109bbd4ee5d700a
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-07-13 13:45:49 +0530

    Time: 7 ms (63.48%) | Memory: 11.9 MB (55.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,23 +5,16 @@ public:
         int i=0;
         int j=0;
         int maxsize=0;
-        unordered_set<char>st;
-        int size=0;
-        while(i<=j && j<n){
-            while(st.find(s[j])!=st.end()){
-                st.erase(s[i]);
-                
-                
-                i=i+1;
-                
-
+        unordered_map<char,int>mp;
+        while(j<n){
+            if(mp.find(s[j])!=mp.end() && i<=mp[s[j]]){
+                i=mp[s[j]]+1;
+                mp[s[j]]=j;
             }
-            st.insert(s[j]);
-            size=j-i+1;
+            mp[s[j]]=j;
+            int size=j-i+1;
             j++;
             maxsize=max(maxsize,size);
-
-
         }
         return maxsize;
         
