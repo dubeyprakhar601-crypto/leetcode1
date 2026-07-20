50623919d934e2077c5ecbc95b79fffb7bd423a2
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-07-20 20:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (94.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& nums) {
+
         int m=nums.size();
         int n=nums[0].size();
-        vector<int>indj;
-
+        int col0=1;
         for(int i=0;i<m;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i][j]==0){
-                    indj.push_back(j);
-                    count++;
-                }
+            if(nums[i][0]==0){
+                col0=0;
+                break;
             }
-            if(count!=0){
-                for(int j=0;j<n;j++){
+        }
+        for(int i=n-1;i>=1;i--){
+            if(nums[0][i]==0){
+                nums[0][0]=0;
+                break;
+            }
+        }
+        
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
+                if(nums[i][j]==0){
+                    nums[i][0]=0;
+                    nums[0][j]=0;
+                }
+
+            }
+        }
+        for(int i=m-1;i>=1;i--){
+            for(int j=n-1;j>=1;j--){
+
+                if(nums[i][0]==0 || nums[0][j]==0){
                     nums[i][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<indj.size();j++){
-                nums[i][indj[j]]=0;
+        
+        for(int i=n-1;i>=1;i--){
+            if(nums[0][0]==0){
+                nums[0][i]=0;
             }
         }
+        for(int i=0;i<m;i++){
+            if(col0==0){
+                nums[i][0]=0;
+            }
+        }
+      
+
+        
         
     }
 };
