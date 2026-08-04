46b1ce4213cb0fb911825a8b777d4635c14a5346
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-04 14:50:46 +0530

    Time: 23 ms (52.48%) | Memory: 70.6 MB (48.48%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+long long noOFBou(vector<int>& nums, int k, int mid){
+    int i=0;
+    int x=0;
+    int ans=0;
+    while(i<nums.size()){
+        if(nums[i]>mid){
+            x=0;
+        }else{
+            x++;
+        }
+        if(x==k){
+            ans+=1;
+            x=0;
+        }
+        i++;
+    }
+    return ans;
+
+}
+    int minDays(vector<int>& nums, int m, int k) {
+        int n=nums.size();
+        if((long long)n <(long long) m*k) return -1;
+        int low=INT_MAX;
+        int high=INT_MIN;
+        for(int i=0;i<n;i++){
+            low=min(low,nums[i]);
+            high=max(high,nums[i]);
+        }
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long x=noOFBou(nums,k,mid);
+            if(x>=m){
+                ans=mid;
+                high=mid-1;
+
+
+            }else{
+                low=mid+1;
+            }
+        }
+        if(ans!=0){
+            return ans;
+        }
+        return -1;
+        
+    }
+};
