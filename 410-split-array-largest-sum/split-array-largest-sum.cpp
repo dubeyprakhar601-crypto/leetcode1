6b4f9a28a9b5e50c413de7bf732da4600c4a7f07
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-08 13:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (43.61%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+bool noOfSubarray(vector<int>& nums, int k,int maxSum){
+    int n=nums.size();
+    int array=1;
+    int sum=0;
+    for(int i=0;i<n;i++){
+       if(sum+nums[i]<=maxSum){
+        sum+=nums[i];
+       }else{
+        array++;
+        sum=nums[i];
+       }
+        
+    }
+    return array<=k;
+}
+    int splitArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        int st=*max_element(nums.begin(),nums.end());
+        int end=0;
+        for(int i=0;i<n;i++){
+            end+=nums[i];
+        }
+        int ans=0;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(noOfSubarray(nums,k,mid)){
+                ans=mid;
+                end=mid-1;
+
+            }else{
+                st=mid+1;
+
+            }
+        }return ans;
+        
+    }
+};
