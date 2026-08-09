d2cd65d2b9363ca6b94991541e00a86016077053
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-09 22:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.97%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -5,42 +5,46 @@ public:
         int n2 = nums2.size();
         int i = 0;
         int j = 0;
-        int count = 0;
+        int count = 1;
         int s1 = 0;
         int s2 = 0;
         int n = n1 + n2;
         while (i < n1 && j < n2) {
             if (nums1[i] < nums2[j]) {
-                count++;
+                
                 if (count == n / 2)
                     s1 = nums1[i];
                 if (count == (n + 2) / 2)
                     s2 = nums1[i];
+                    count++;
                 i++;
 
             } else {
-                count++;
+                
                 if (count == n / 2)
                     s1 = nums2[j];
                 if (count == (n + 2) / 2)
                     s2 = nums2[j];
+                    count++;
                 j++;
             }
         }
         while (i < n1) {
-            count++;
+           
             if (count == n / 2)
                 s1 = nums1[i];
             if (count == (n + 2) / 2)
                 s2 = nums1[i];
+                 count++;
             i++;
         }
         while (j < n2) {
-            count++;
+            
             if (count == n / 2)
                 s1 = nums2[j];
             if (count == (n + 2) / 2)
                 s2 = nums2[j];
+                count++;
             j++;
         }
         if(n%2==0){
