038425991e19b8e59e963296b234d30b5cfc70f6
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-10 14:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-bool columnSearch(vector<vector<int>>& nums, int target,int  row,int n){
-    int st=0;
-    int end=n-1;
-    while(st<=end){
-        int mid=st+(end-st)/2;
-        if(nums[row][mid]==target){
-            return true;
-        }else if(nums[row][mid]<target){
-            st=mid+1;
-        }else{
-            end=mid-1;
-        }
-    }
-    return false;
-
-}
-int rowSearch(vector<vector<int>>& nums, int target,int m){
-    int st=0;
-    int end=m-1;
-    int ans=-1;
-
-    while(st<=end){
-        int mid=st+(end-st)/2;
-
-         if(nums[mid][0]<=target){
-            ans=mid;
-            st=mid+1;
-        }else{
-            end=mid-1;
-
-        }
-    }
-    return ans;
-}
     bool searchMatrix(vector<vector<int>>& nums, int target) {
         int m=nums.size();
         int n=nums[0].size();
-        if(target<nums[0][0] || target>nums[m-1][n-1]){
-            return false;
+        int st=0;
+        int end=(m*n)-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            int row=mid/n;
+            int column=mid%n;
+            if(nums[row][column]==target){
+                return true;
+            }else if(target<nums[row][column]){
+                end=mid-1;
+            }else{
+                st=mid+1;
+            }
         }
-        int row=rowSearch(nums,target,m);
-        return columnSearch(nums, target, row, n);
-
-
+        return false;
         
     }
 };
