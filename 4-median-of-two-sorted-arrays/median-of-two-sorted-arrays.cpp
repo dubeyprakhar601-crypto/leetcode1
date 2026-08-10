a46180320abb015241ac565bad761b821cd479d2
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-10 09:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (80.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,55 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int i = 0;
-        int j = 0;
-        int count = 1;
-        int s1 = 0;
-        int s2 = 0;
-        int n = n1 + n2;
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
-                
-                if (count == n / 2)
-                    s1 = nums1[i];
-                if (count == (n + 2) / 2)
-                    s2 = nums1[i];
-                    count++;
-                i++;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int n=(n1+n2);
+        int left=(n1+n2+1)/2;
+        int low=0;
+        int high=n1;
+        while(low<=high){
+            int mid1=low+(high-low)/2;
+            int mid2= left-mid1;
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int r1=INT_MAX;
+            int r2=INT_MAX;
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1) return max(l1,l2);
+                else{
+                    return (double)(max(l1,l2)+min(r1,r2))/2.0;
+                }
+            }else if(l1>r2){
+                high=mid1-1;
 
-            } else {
-                
-                if (count == n / 2)
-                    s1 = nums2[j];
-                if (count == (n + 2) / 2)
-                    s2 = nums2[j];
-                    count++;
-                j++;
+            }else{
+                low=mid1+1;
+
             }
         }
-        while (i < n1) {
-           
-            if (count == n / 2)
-                s1 = nums1[i];
-            if (count == (n + 2) / 2)
-                s2 = nums1[i];
-                 count++;
-            i++;
-        }
-        while (j < n2) {
-            
-            if (count == n / 2)
-                s1 = nums2[j];
-            if (count == (n + 2) / 2)
-                s2 = nums2[j];
-                count++;
-            j++;
-        }
-        if(n%2==0){
-            return (double)(s1+s2)/2;
-        }
-        return s2;
+        return 0;
+        
     }
 };
