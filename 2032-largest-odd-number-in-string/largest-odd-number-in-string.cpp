329e736c51b875f1f886044df449214ed885bfab
Prakhar Dubey <dubeyprakhar601@gmail.com>
2026-08-13 09:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.26%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -5,12 +5,10 @@ public:
         int n=nums.length();
         string ans="";
         for(int i=n-1;i>=0;i--){
-            if(nums[i]%2==0){
-                nums.pop_back();
+            if(nums[i]%2==1){
+                return nums.substr(0,i+1);
             }
-            else{
-                return nums;
-            }
+            
         }
         return ans;
     }
